b97a66a92ff22faf444dc32d2a2075bcf3dc6449
Shubh <Shubham@Venom.com>
2025-05-08 08:42:18 +0530

    Java HelloWorldApp.java

---
+++ HelloWorldApp.java
@@ -0,0 +1,6 @@
+// Basic Java program to print "Hello, World!" to the console
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! This is a basic Java program.");
+    }
+}
